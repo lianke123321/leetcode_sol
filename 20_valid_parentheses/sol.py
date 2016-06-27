48969337ad2b6c6d6af23e2a166dc0e8b9a7652c
adrian <ankeli@cs.stonybrook.edu>
2016-06-27 15:03:40 -0400

    completed valid parentheses

---
+++ 20_valid_parentheses/sol.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        dict = {']': '[', '}': '{', ')': '('}
+        for char in s:
+            if char in dict.values():
+                stack.append(char)
+            elif char in dict.keys():
+                if stack == [] or dict[char] != stack.pop():
+                    return False
+            else:
+                return False
+        return stack == []
+
+    def isValid_self(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        right_to_left = {']': '[',
+                         '}': '{',
+                         ')': '('}
+        stack = []
+        for char in s:
+            if char in right_to_left and (len(stack) == 0 or right_to_left[char] != stack.pop()):
+                return False
+            if char in right_to_left.values():
+                stack.append(char)
+        return stack == []
+
+sol = Solution()
+print sol.isValid_self('()')
