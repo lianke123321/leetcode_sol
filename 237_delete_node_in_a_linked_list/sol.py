1a44ec1403e3689cea50a316bea750062d336caf
adrian <ankeli@cs.stonybrook.edu>
2016-06-29 14:29:11 -0400

    completed delete_node_in_a_linked_list

---
+++ 237_delete_node_in_a_linked_list/sol.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        temp = node.next
+        node.next = node.next.next
+        temp.next = None
+
+    def deleteNode_self(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
