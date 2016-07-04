8fcc15e24888b10feb3978bd6438c65b3435da4d
adrian <ankeli@cs.stonybrook.edu>
2016-07-04 17:27:00 -0400

    completed find_min_in_rotated_sorted_array_2

---
+++ 154_find_minimum_in_rotated_sorted_array_2/sol.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        lo = 0
+        hi = len(nums) - 1
+        while lo < hi:
+            if nums[lo] < nums[hi]:
+                return nums[lo]
+            mid = lo + (hi - lo) / 2
+            if nums[mid] > nums[hi]:
+                lo = mid + 1
+            elif nums[mid] < nums[hi]:
+                hi = mid
+            else:
+                hi -= 1
+        return nums[lo]
+
+    def findMin_self(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        lo = 0
+        hi = len(nums) - 1
+        while lo < hi:
+            if nums[lo] < nums[hi]:
+                return nums[lo]
+
+            mid = (lo + hi) / 2
+            if nums[mid] > nums[hi]:
+                lo = mid + 1
+            elif nums[mid] < nums[hi]:
+                hi = mid
+            else:
+                hi -= 1
+        return nums[lo]
