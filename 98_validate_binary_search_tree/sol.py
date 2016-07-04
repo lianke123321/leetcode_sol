df469781ce8b0453a7dfecd7d8c924da0101ba96
adrian <ankeli@cs.stonybrook.edu>
2016-07-04 13:01:41 -0400

    completed validate_BST

---
+++ 98_validate_binary_search_tree/sol.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        result = True
+        stack = []
+        p = root
+        pre = None
+        while (p != None and p.val != '#') or stack:
+            if p != None and p.val != '#':
+                stack.append(p)
+                p = p.left
+            else:
+                temp = stack.pop()
+                if (pre != None and pre.val != '#') and temp.val <= pre.val:
+                    return False
+                pre = temp
+                p = temp.right
+        return result
+
+    def isValidBST_self(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        stack = []
+        p = root
+        pre = None
+        while p or stack:
+            if p:
+                stack.append(p)
+                p = p.left
+            else:
+                curr = stack.pop()
+                if pre and curr.val <= pre.val:
+                    return False
+                pre = curr
+                p = curr.right
+        return True
+
