199ff94b10234f998026d7556e06e0fe755e2418
adrian <ankeli@cs.stonybrook.edu>
2016-07-05 13:57:00 -0400

    completed H-index

---
+++ 274_H-index/sol.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        if not citations:
+            return 0
+        length = len(citations)
+        h = 0
+        count = [0 for i in range(length + 1)]
+        for c in citations:
+            count[min(c, length)] += 1
+        for i in range(length + 1):
+            h += count[length - i]
+            if h >= length - i:
+                return length - i
+
+    def hIndex_self(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        length = len(citations)
+        if length == 0:
+            return 0
+
+        tmp = [0] * (length + 1)
+
+        for c in citations:
+            if c > length:
+                tmp[-1] += 1
+            else:
+                tmp[c] += 1
+
+        h = 0
+        i = length
+        while i >= 0:
+            h += tmp[i]
+            if h >= i:
+                return i
+            i -= 1
+
+sol = Solution()
+print sol.hIndex_self([1, 2])
