e5e2749f64e4f0318959cc07ba226511f1218826
adrian <ankeli@cs.stonybrook.edu>
2016-07-05 12:20:57 -0400

    completed valid_perfect_square

---
+++ 367_valid_perfect_square/sol.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        lo = 1
+        hi = num
+        while lo <= hi:
+            mid = (lo + hi) / 2
+            if mid * mid > num:
+                hi = mid - 1
+            elif mid * mid < num:
+                lo = mid + 1
+            else:
+                return True
+
+        return False
+
+
+sol = Solution()
+print sol.isPerfectSquare(5)
