bba54139a923d3730bd63c545c514684447c5751
adrian <ankeli@cs.stonybrook.edu>
2016-07-14 15:37:30 -0400

    completed longest_increasing_subsequence

---
+++ 300_longest_increasing_subsequence/sol.py
@@ -0,0 +1,20 @@
+# no Liana solution
+
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+
+        dp = [1] * len(nums)
+        result = 1
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+            result = max(result, dp[i])
+        return result
