04b833307e914b2030d2a3a0354e7c208a4d9aff
adrian <ankeli@cs.stonybrook.edu>
2016-07-18 18:12:24 -0400

    completed unique_paths

---
+++ 62_unique_paths/sol.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[0 for i in range(n)] for i in range(m)]
+        for i in range(0, n):
+            dp[0][i] = 1
+        for j in range(1, m):
+            dp[j][0] = 1
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[m - 1][n - 1]
+
+    def uniquePaths_self(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[1 for _ in xrange(n)] for _ in xrange(m)]
+
+        for i in xrange(1, m):
+            for j in xrange(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[-1][-1]
