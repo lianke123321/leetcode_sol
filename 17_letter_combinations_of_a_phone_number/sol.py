363edcba2a354605cf0c464ef8e72ded6fd1bc3a
adrian <ankeli@cs.stonybrook.edu>
2016-07-26 16:16:53 -0400

    completed letter_combinations_of_a_phone_number

---
+++ 17_letter_combinations_of_a_phone_number/sol.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        hashMap = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+        if not digits:
+            return []
+        res = ['']
+        for i in range(0, len(digits)):
+            while res and len(res[0]) == i:
+                for c in hashMap[digits[i]]:
+                    res += [res[0] + c]
+                del res[0]
+        return res
+
+    def letterCombinations_self(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+        num_to_letters = {'2': 'abc', '3': 'def', '4': 'ghi',
+                          '5': 'jkl', '6': 'mno', '7': 'pqrs',
+                          '8': 'tuv', '9': 'wxyz'}
+        result = ['']
+        if not digits:
+            return result
+        for i in xrange(len(digits)):
+            while len(result[0]) == i:
+                for letter in num_to_letters[digits[i]]:
+                    result.append(result[0] + letter)
+                del result[0]
+        return result
