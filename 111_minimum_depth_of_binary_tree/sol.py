dddd394718205ac3d0e89e35e843790448d5151d
adrian <ankeli@cs.stonybrook.edu>
2016-07-25 22:24:01 -0400

    completed minimum_depth_of_binary_tree

---
+++ 111_minimum_depth_of_binary_tree/sol.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        left = self.minDepth(root.left)
+        right = self.minDepth(root.right)
+        if left == 0 or right == 0:
+            return left + right + 1
+        else:
+            return min(left, right) + 1
+
+    def minDepth_self(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        min_left = self.minDepth_self(root.left)
+        min_right = self.minDepth_self(root.right)
+        if min_left == 0 or min_right == 0:
+            return min_left + min_right + 1
+        else:
+            return min(min_left, min_right) + 1
