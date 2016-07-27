1882b29975e9e5385a1b8231d5f716f28cdf6435
adrian <ankeli@cs.stonybrook.edu>
2016-07-27 13:58:29 -0400

    completed combination_sum

---
+++ 39_combination_sum/sol.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        candidates.sort()
+        stack = [(0, 0, [])]
+        result = []
+        while stack:
+            total, start, res = stack.pop()
+            if total == target:
+                result.append(res)
+            for i in range(start, len(candidates)):
+                t = total + candidates[i]
+                if t > target:
+                    break
+                stack.append((t, i, res + [candidates[i]]))
+        return result
+
+
+class Solution_self(object):
+    def __init__(self):
+        self.result = []
+
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        self.dfs([], candidates, target)
+        return self.result
+
+    def dfs(self, stack, candidates, target):
+        if target == 0:
+            self.result.append(stack)
+        else:
+            for i in reversed(xrange(len(candidates))):
+                if candidates[i] > target:
+                    continue
+                self.dfs(stack + [candidates[i]], candidates[:i + 1], target - candidates[i])
