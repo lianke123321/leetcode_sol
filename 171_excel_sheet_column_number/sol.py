fd7cac6edf2ad442bdd2053406933351e040996d
adrian <ankeli@cs.stonybrook.edu>
2016-07-28 23:08:55 -0400

    excel_sheet_column_number

---
+++ 171_excel_sheet_column_number/sol.py
@@ -0,0 +1,16 @@
+# no Liana solution
+
+
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result = 0
+        for i in reversed(xrange(len(s))):
+            result += (ord(s[i]) - ord('A') + 1) * (26 ** (len(s) - i - 1))
+        return result
+
+sol = Solution()
+print sol.titleToNumber('AB')
