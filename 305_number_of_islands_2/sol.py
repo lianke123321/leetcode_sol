e16cbb72cbf9b353572a3832b1e650250c8fba10
adrian <ankeli@cs.stonybrook.edu>
2016-08-10 13:25:16 -0400

    completed number_of_islands_2

---
+++ 305_number_of_islands_2/sol.py
@@ -0,0 +1,49 @@
+# no Liana solution
+
+
+class Solution(object):
+    def numIslands2(self, m, n, positions):
+        """
+        :type m: int
+        :type n: int
+        :type positions: List[List[int]]
+        :rtype: List[int]
+        """
+        result = []
+        union = Union()
+        for p in map(tuple, positions):
+            union.add(p)
+            for x in (0, 1), (0, -1), (1, 0), (-1, 0):
+                q = (p[0] + x[0], p[1] + x[1])
+                if q in union.id:
+                    union.unite(p, q)
+            result.append(union.count)
+        return result
+
+
+class Union(object):
+    def __init__(self):
+        self.id = {}
+        self.sz = {}
+        self.count = 0
+
+    def add(self, p):
+        self.id[p] = p
+        self.sz[p] = 1
+        self.count += 1
+
+    def root(self, p):
+        while p != self.id[p]:
+            self.id[p] = self.id[self.id[p]]
+            p = self.id[p]
+        return p
+
+    def unite(self, p, q):
+        i, j = self.root(p), self.root(q)
+        if i == j:
+            return
+        if self.sz[i] > self.sz[j]:
+            i, j = j, i
+        self.id[i] = j
+        self.sz[j] += self.sz[i]
+        self.count -= 1
