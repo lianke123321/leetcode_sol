ed8f202d38cb54e161756529b488110a8905814c
adrian <ankeli@cs.stonybrook.edu>
2016-08-17 12:33:01 -0400

    completed 71_simplify_path

---
+++ 71_simplify_path/sol.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        stack = []
+        for item in path.split("/"):
+            if item not in [".", "..", ""]:
+                stack.append(item)
+            if item == ".." and stack:
+                stack.pop()
+        return "/" + "/".join(stack)
+
+    def simplifyPath_self(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        stack = []
+        special = {'.', '..', ''}
+        for item in path.split('/'):
+            if item not in special:
+                stack.append(item)
+            if item == '..' and stack:
+                stack.pop()
+        return '/' + '/'.join(stack)
