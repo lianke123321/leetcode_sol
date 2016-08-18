825936f76c008e6493c0703190f590715f8b540a
adrian <ankeli@cs.stonybrook.edu>
2016-08-18 17:42:44 -0400

    added iterative solution to Q21

--- 21_merge_two_sorted_list/sol.py
+++ 21_merge_two_sorted_list/sol.py
@@ -39,3 +39,32 @@ class Solution:
         else:
             l1.next = self.mergeTwoLists(l1.next, l2)
             return l1
+
+    def mergeTwoLists_self_iteration(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        dummy = ListNode(0)
+        p = dummy
+        while l1 or l2:
+            if l1 and l2:
+                if l1.val <= l2.val:
+                    p.next = l1
+                    l1 = l1.next
+                else:
+                    p.next = l2
+                    l2 = l2.next
+            elif l1:
+                p.next = l1
+                l1 = l1.next
+            else:
+                p.next = l2
+                l2 = l2.next
+            p = p.next
+        return dummy.next
