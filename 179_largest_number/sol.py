5a6c172cfd1b431de20e48fac718c4d79449b18c
adrian <ankeli@cs.stonybrook.edu>
2016-09-02 16:02:11 -0400

    revised Q179 sol

--- 179_largest_number/sol.py
+++ 179_largest_number/sol.py
@@ -11,7 +11,4 @@ class Solution:
         nums = [str(x) for x in nums]
         nums.sort(cmp=lambda x, y: cmp(y + x, x + y))
         result = ''.join(nums).lstrip('0')
-        if len(result) > 0:
-            return result
-        else:
-            return "0"
+        return result if len(result) > 0 else "0"
