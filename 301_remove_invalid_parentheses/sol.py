ed1d997e32ce91936b7ac8939a08057385129c73
adrian <ankeli@cs.stonybrook.edu>
2016-09-12 15:09:44 -0400

    completed remove_invalid_parentheses

---
+++ 301_remove_invalid_parentheses/sol.py
@@ -0,0 +1,44 @@
+# no Liana solution
+
+
+class Solution(object):
+    def removeInvalidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        from collections import deque
+        result = []
+
+        visited, queue, found = {s}, deque(), False
+        queue.append(s)
+        while queue:
+            candidate = queue.pop()
+            if self.is_valid(candidate):
+                result.append(candidate)
+                found = True
+
+            if found:
+                continue
+
+            for i in xrange(len(candidate)):
+                if candidate[i] == '(' or candidate[i] == ')':
+                    child = candidate[:i] + candidate[i + 1:]
+                    if child not in visited:
+                        queue.appendleft(child)
+                        visited.add(child)
+        return result
+
+    def is_valid(self, s):
+        count = 0
+        for c in s:
+            if c == '(':
+                count += 1
+            elif c == ')':
+                if count == 0:
+                    return False
+                count -= 1
+        return count == 0
+
+sol = Solution()
+print sol.removeInvalidParentheses('()())()')
