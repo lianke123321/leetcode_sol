97aae2f7e81b2650eac6add36d1ba5efce9445b8
adrian <ankeli@cs.stonybrook.edu>
2016-09-12 15:29:04 -0400

    completed subsets

---
+++ 78_subsets/sol.py
@@ -0,0 +1,35 @@
+class Solution:
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if nums == []:
+            return []
+        nums.sort() #sort the array to avoid descending list of int
+        res = [[]]
+        for element in nums:
+            temp = []
+            for ans in res:
+                 #append the new int to each existing list
+                temp.append(ans + [element])
+            res += temp
+        return res
+
+    def subsets_self(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = [[]]
+        if not nums:
+            return result
+        for n in nums:
+            tmp = []
+            for ans in result:
+                tmp.append(ans + [n])
+            result += tmp
+        return result
+
+sol = Solution()
+print sol.subsets_self([1,4,3])
