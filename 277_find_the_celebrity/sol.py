891e94693ae5e2b8e54f06f5c540c9419f2f4df9
adrian <ankeli@cs.stonybrook.edu>
2016-09-13 16:01:36 -0400

    completed find_the_celebrity

---
+++ 277_find_the_celebrity/sol.py
@@ -0,0 +1,28 @@
+# no Liana solution
+
+
+# The knows API is already defined for you.
+# @param a, person a
+# @param b, person b
+# @return a boolean, whether a knows b
+def knows(a, b):
+    return True
+
+
+class Solution(object):
+    def findCelebrity(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        candidate = 0
+        for i in xrange(n):
+            if knows(candidate, i):
+                candidate = i
+        for i in xrange(n):
+            if i != candidate and knows(candidate, i):
+                return -1
+        for i in xrange(n):
+            if not knows(i, candidate):
+                return -1
+        return candidate
