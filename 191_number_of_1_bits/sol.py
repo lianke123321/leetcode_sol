0da9f45d49759585f825963e7f10550cf6e273c0
adrian <ankeli@cs.stonybrook.edu>
2016-09-20 18:45:17 -0400

    added another sol to Q191

--- 191_number_of_1_bits/sol.py
+++ 191_number_of_1_bits/sol.py
@@ -12,3 +12,14 @@ class Solution(object):
             res += n & 1
             n >>= 1
         return res
+
+    def hammingWeight_2(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res = 0
+        while n > 0:
+            n &= (n - 1)
+            res += 1
+        return res
