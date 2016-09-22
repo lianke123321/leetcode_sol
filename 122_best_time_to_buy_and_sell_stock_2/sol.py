94a6abbe8e4012ef61fd240c228b7c1ae0464087
adrian <ankeli@cs.stonybrook.edu>
2016-09-21 23:18:01 -0400

    optimized Q122 sol to be O(1) space complexity

--- 122_best_time_to_buy_and_sell_stock_2/sol.py
+++ 122_best_time_to_buy_and_sell_stock_2/sol.py
@@ -21,3 +21,21 @@ class Solution(object):
                 dp[i] = dp[i - 1]
 
         return dp[-1]
+
+    def maxProfit_O_1_space(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        count = len(prices)
+
+        if count < 2:
+            return 0
+
+        profit = 0
+
+        for i in xrange(1, count):
+            if prices[i] >= prices[i - 1]:
+                profit += (prices[i] - prices[i - 1])
+
+        return profit
