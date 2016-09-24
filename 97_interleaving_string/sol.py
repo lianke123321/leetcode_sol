b91021a50ba9a7a0e5262d86247ce58694224e41
adrian <ankeli@cs.stonybrook.edu>
2016-09-24 15:11:36 -0400

    slight improvement for Q97 sol

--- 97_interleaving_string/sol.py
+++ 97_interleaving_string/sol.py
@@ -16,16 +16,21 @@ class Solution(object):
         dp[0][0] = True
 
         for i in xrange(1, len(s1) + 1):
-            dp[i][0] = dp[i - 1][0] and s1[i - 1] == s3[i - 1]
+            if not dp[i - 1][0]:
+                break
+            dp[i][0] = s1[i - 1] == s3[i - 1]
 
         for j in xrange(1, len(s2) + 1):
-            dp[0][j] = dp[0][j - 1] and s2[j - 1] == s3[j - 1]
+            if not dp[0][j - 1]:
+                break
+            dp[0][j] = s2[j - 1] == s3[j - 1]
 
         for i in xrange(1, len(s1) + 1):
             for j in xrange(1, len(s2) + 1):
                 dp[i][j] = (dp[i - 1][j] and s1[i - 1] == s3[i + j - 1]) or \
                            (dp[i][j - 1] and s2[j - 1] == s3[i + j - 1])
         return dp[-1][-1]
+
 
 sol = Solution()
 print sol.isInterleave('aa', 'ab', 'aaba')
