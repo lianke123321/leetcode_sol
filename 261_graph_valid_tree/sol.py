596adb09939f931c0342e87db75d9b3424a4fc0a
adrian <ankeli@cs.stonybrook.edu>
2016-09-28 14:40:48 -0400

    revised Q261 sol

--- 261_graph_valid_tree/sol.py
+++ 261_graph_valid_tree/sol.py
@@ -8,17 +8,17 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: bool
         """
+        if len(edges) != n - 1:
+            return False
+
         nums = [-1] * n
         for edge in edges:
             x = self.find(nums, edge[0])
             y = self.find(nums, edge[1])
-
             if x == y:
                 return False
-
             nums[x] = y
-
-        return len(edges) == n - 1
+        return True
 
     def find(self, nums, i):
         if nums[i] == -1:
