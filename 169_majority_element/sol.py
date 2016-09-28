e7587a912cbf5c57428c99fdea8fe3f449b76202
adrian <ankeli@cs.stonybrook.edu>
2016-09-28 13:55:15 -0400

    added bit manipulation sol to Q169

--- 169_majority_element/sol.py
+++ 169_majority_element/sol.py
@@ -31,3 +31,25 @@ class Solution(object):
             else:
                 count -= 1
         return result
+
+    def majorityElement_bit_manipulation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        bit_counter = [0] * 32
+        for n in nums:
+            binary = bin(n)[2:]
+            j = 0
+            for i in reversed(xrange(len(binary))):
+                if binary[i] == '1':
+                    bit_counter[j] += 1
+                j += 1
+
+        res = ''
+        for c in bit_counter:
+            res = '1' + res if c > len(nums) / 2 else '0' + res
+        return int(res.lstrip('0'), 2)
+
+sol = Solution()
+print sol.majorityElement_bit_manipulation([1, 3, 5, 5, 5])
