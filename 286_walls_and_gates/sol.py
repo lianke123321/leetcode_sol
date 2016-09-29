434d41ab481737fc1d9230bcd1da6f072beede14
adrian <ankeli@cs.stonybrook.edu>
2016-09-29 14:24:34 -0400

    simplified Q286 sol

--- 286_walls_and_gates/sol.py
+++ 286_walls_and_gates/sol.py
@@ -22,15 +22,7 @@ class Solution(object):
         while queue:
             top = queue.pop()
             row, col = top[0], top[1]
-            if row > 0 and rooms[row - 1][col] == 2147483647:
-                rooms[row - 1][col] = rooms[row][col] + 1
-                queue.appendleft((row - 1, col))
-            if row < n - 1 and rooms[row + 1][col] == 2147483647:
-                rooms[row + 1][col] = rooms[row][col] + 1
-                queue.appendleft((row + 1, col))
-            if col > 0 and rooms[row][col - 1] == 2147483647:
-                rooms[row][col - 1] = rooms[row][col] + 1
-                queue.appendleft((row, col - 1))
-            if col < m - 1 and rooms[row][col + 1] == 2147483647:
-                rooms[row][col + 1] = rooms[row][col] + 1
-                queue.appendleft((row, col + 1))
+            for r, c in (row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1):
+                if 0 <= r < n and 0 <= c < m and rooms[r][c] == 2147483647:
+                    rooms[r][c] = rooms[row][col] + 1
+                    queue.appendleft((r, c))
