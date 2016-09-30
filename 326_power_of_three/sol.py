199805816c8048000ac0f10b23b6ecd9e0314310
adrian <ankeli@cs.stonybrook.edu>
2016-09-30 00:04:26 -0400

    completed power_of_three

---
+++ 326_power_of_three/sol.py
@@ -0,0 +1,10 @@
+# no Liana solution
+
+
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n > 0 and 1162261467 % n == 0
