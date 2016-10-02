31300957ab8b7c0daaa9ab6a1469c3862cc93337
adrian <ankeli@cs.stonybrook.edu>
2016-10-02 17:50:03 -0400

    updated O(1) sol for Q70

--- 70_climbing_stairs/sol.py
+++ 70_climbing_stairs/sol.py
@@ -18,9 +18,9 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp = [0 for _ in range(n + 1)]
-        dp[0] = 1
-        dp[1] = 1
-        for i in range(2, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-        return dp[n]
+        prev = curr = 1
+        for _ in xrange(n - 1):
+            tmp = curr
+            curr += prev
+            prev = tmp
+        return curr
