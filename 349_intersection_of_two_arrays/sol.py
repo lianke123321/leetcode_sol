9cb050a77f1930350d722b5c6350cb8ed3e2f65a
adrian <ankeli@cs.stonybrook.edu>
2016-10-02 16:19:14 -0400

    revised Q349 sol

--- 349_intersection_of_two_arrays/sol.py
+++ 349_intersection_of_two_arrays/sol.py
@@ -8,6 +8,4 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        nums1 = set(nums1)
-        nums2 = set(nums2)
-        return list(nums1 & nums2)
+        return list(set(nums1) & set(nums2))
